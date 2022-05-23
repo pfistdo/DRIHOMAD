9bd608494b1301ff8af24a7bb8c5e1e821f124af
pfistdo1 <pfistdo1@students.zhaw.ch>
2022-05-23 19:25:07 +0200

    REST in python

--- histogram.py
+++
@@ -1,8 +0,0 @@
-# add the following Python code to the new file:
-import matplotlib.pyplot as plt
-import numpy as np
-rng = np.random.RandomState(10)
-a = np.hstack((rng.normal(size=1000)))
-plt.hist(a, bins='auto', color='green')
-plt.title("Histogram with 'auto' bins")
-plt.show()
