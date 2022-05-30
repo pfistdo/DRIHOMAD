9321b30f5ce394c4b1c5d07991d0d7b30e8c4eeb
pfistdo1 <pfistdo1@students.zhaw.ch>
2022-05-30 20:15:57 +0200

    REST call incl. flask

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # DRIHOMAD
 
 Group project for scientific programming.
+
+Run with 'python -m flask' run or with IDE
+Enable debug mode in config_flask.py with 'DEBUG = True'
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import json
+import requests
+
+app = Flask(__name__)
+app.config.from_object('config_flask') #load config
+
+drivers2 = ["VER", "LEC", "SAI"] #test only
+
+@app.route("/")
+def driver():
+    return render_template("pages/drivers.html")
+
+@app.route('/drivers/result', methods=['GET'])
+def driversByYear() -> str:
+    app.logger.info(request.args.get('year'))
+    year = str(request.args.get('year'))
+    url = "http://ergast.com/api/f1/" + year + "/drivers.json"
+    app.logger.info(url)
+    response = requests.get(url)
+    allDrivers = json.loads(response.text)
+    drivers = allDrivers["MRData"]["DriverTable"]["Drivers"]
+    return render_template(template_name_or_list='pages/driversResult.html', drivers=drivers)
+
+@app.route("/test") #test only 
+def home():
+    return render_template("test.html", title="Website title", drivers=drivers2)
+
+if __name__ == "__main__":
+    app.run()
